a09f59b5b1e98317658022661a05f7f8f7c9631f
Yuli <yu.expert@gmail.com>
2025-07-17 23:53:58 +0300

    style: add styles for 1920x1080 & 2560x1440 screens, add global transition

--- src/components/Partners/Partners.tsx
+++ src/components/Partners/Partners.tsx
@@ -1,77 +1,77 @@
 const Partners = () => {
   return (
-    <section className="section mb-[228px] 1xl:mb-[250px]">
+    <section className="section mb-[228px] 1xl:mb-[250px] 3xl:mb-[337px] 4xl:mb-[370px]">
       <div className="pt-7.5 1xl:pt-0">
-        <h2 className="mb-10.5 1xl:mb-12 text-mercury-white text-[34px]/[110%] 1xl:text-[54px] tracking-[-0.02em] font-second text-center 1xl:text-left">
+        <h2 className="mb-10.5 1xl:mb-12 3xl:mb-14.5 4xl:mb-15 text-mercury-white text-[34px]/[110%] 1xl:text-[54px] 4xl:text-[62px] tracking-[-0.02em] font-second text-center 1xl:text-left">
           Наші партнери
         </h2>
-        <ul className="grid grid-cols-2 1xl:grid-cols-3 gap-x-2 1xl:gap-3 gap-y-2.5">
-          <li className="group flex items-center justify-center bg-cod-dark rounded-lg">
-            <svg className="w-[162px] 1xl:w-[273px] h-4 1xl:h-7 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
+        <ul className="grid grid-cols-2 1xl:grid-cols-3 3xl:grid-cols-4 gap-x-2 1xl:gap-3 3xl:gap-x-3.5 4xl:gap-x-2 gap-y-2.5 3xl:gap-y-4.5 4xl:gap-y-4.5">
+          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px] 4xl:h-[172px]">
+            <svg className="w-[162px] 1xl:w-[272px] h-4 1xl:h-7 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
               <use href="/images/svg/icons.svg#icon-unmoment" />
             </svg>
           </li>
-          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px]">
+          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px] 4xl:h-[172px]">
             <svg className="w-20 1xl:w-[118px] h-6 1xl:h-9 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
               <use href="/images/svg/icons.svg#icon-mega-market" />
             </svg>
           </li>
-          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px]">
-            <svg className="w-[142px] 1xl:w-[197px] h-7.5 1xl:h-11 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
+          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px] 4xl:h-[172px]">
+            <svg className="w-[142px] 1xl:w-[198px] h-7.5 1xl:h-11 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
               <use href="/images/svg/icons.svg#icon-stroyRent" />
             </svg>
           </li>
-          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px]">
-            <svg className="w-18.5 1xl:w-[110px] h-6.5 1xl:h-9.5 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
+          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px] 4xl:h-[172px]">
+            <svg className="w-18.5 1xl:w-[110px] 4xl:w-[134px] h-6.5 1xl:h-9.5 4xl:h-12 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
               <use href="/images/svg/icons.svg#icon-ukrZaliznitsa" />
             </svg>
           </li>
-          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px]">
-            <svg className="w-[143px] 1xl:w-[213px] h-7.5 1xl:h-11 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
+          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px] 4xl:h-[172px]">
+            <svg className="w-[142px] 1xl:w-[212px] 4xl:w-[260px] h-7.5 1xl:h-11 4xl:h-14 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
               <use href="/images/svg/icons.svg#icon-velmart" />
             </svg>
           </li>
-          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px]">
-            <svg className="w-[107px] 1xl:w-[161px] h-11 1xl:h-15.5 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
+          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px] 4xl:h-[172px]">
+            <svg className="w-[108px] 1xl:w-[160px] 4xl:w-[196px] h-11 1xl:h-15.5 4xl:h-20 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
               <use href="/images/svg/icons.svg#icon-silpo" />
             </svg>
           </li>
-          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px]">
-            <svg className="w-[157px] 1xl:w-[224px] h-11 1xl:h-15 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
+          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px] 4xl:h-[172px]">
+            <svg className="w-[158px] 1xl:w-[224px] h-11 1xl:h-15 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
               <use href="/images/svg/icons.svg#icon-Odesa" />
             </svg>
           </li>
-          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px]">
+          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px] 4xl:h-[172px]">
             <svg className="w-[102px] 1xl:w-[150px] h-10.5 1xl:h-15.5 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
               <use href="/images/svg/icons.svg#icon-TORY" />
             </svg>
           </li>
-          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px]">
+          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px] 4xl:h-[172px]">
             <svg className="w-18 1xl:w-25 h-10.5 1xl:h-14.5 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
               <use href="/images/svg/icons.svg#icon-sneakerMate" />
             </svg>
           </li>
-          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px]">
-            <svg className="w-[142px] 1xl:w-[211px] h-4 1xl:h-6 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
+          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px] 4xl:h-[172px]">
+            <svg className="w-[142px] 1xl:w-[210px] h-4 1xl:h-6 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
               <use href="/images/svg/icons.svg#icon-selina" />
             </svg>
           </li>
-          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px]">
-            <svg className="w-[124px] 1xl:w-[176px] h-7 1xl:h-9.5 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
+          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px] 4xl:h-[172px]">
+            <svg className="w-[124px] 1xl:w-[176px] 4xl:w-[214px] h-7 1xl:h-9.5 4xl:h-12 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
               <use href="/images/svg/icons.svg#icon-varus" />
             </svg>
           </li>
-          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px] text-silver-chalice hover:text-tangerine fill-blue transition-colors duration-300">
-            <svg className="w-[104px] 1xl:w-[164px] h-9.5 1xl:h-16.5 fill-current-color ">
+          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px] 4xl:h-[172px] text-silver-chalice hover:text-tangerine transition-colors duration-300">
+            <svg className="w-[104px] 1xl:w-[164px] 4xl:w-[200px] h-9.5 1xl:h-16.5 4xl:h-18 fill-current-color ">
               <use href="/images/svg/icons.svg#icon-dream" />
             </svg>
           </li>
-          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px]">
-            <svg className="w-[138px] 1xl:w-[211px] h-15 1xl:h-22.5 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
+          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px] 4xl:h-[172px]">
+            <svg className="w-[138px] 1xl:w-[210px] h-15 1xl:h-22.5 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
               <use href="/images/svg/icons.svg#icon-Lviv" />
             </svg>
           </li>
-          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px]">
+          <li className="group flex items-center justify-center bg-cod-dark rounded-lg h-[108px] 1xl:h-[132px] 4xl:h-[172px]">
             <svg className="w-[116px] 1xl:w-[186px] h-15 1xl:h-23.5 fill-silver-chalice group-hover:fill-tangerine transition-colors duration-300">
               <use href="/images/svg/icons.svg#icon-Teremki" />
             </svg>
