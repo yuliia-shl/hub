0c0ce84e6342ecb37e8e0c6bc4d8e263996663a7
kapillar <kapillarua@gmail.com>
2025-07-17 08:50:42 +0300

    style: add hover for nav-menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,14 +8,14 @@ const Header = () => {
           </svg>
         </a>
         <nav className="xs:hidden 1xl:block">
-          <ul className="flex 1xl:gap-x-31 1xl:text-xl 3xl:gap-38 4xl:gap-52 4xl:text-2xl text-  items-center h-full">
-            <li>
+          <ul className="flex 1xl:gap-x-31 1xl:text-xl 3xl:gap-38 4xl:gap-52 4xl:text-2xl text-boulder-dust/77 font-bold items-center h-full">
+            <li className="hover:text-gallery-white hover:text-exstrabold">
               <a href="#">Інвестору</a>
             </li>
-            <li>
+            <li className="hover:text-gallery-white hover:text-exstrabold">
               <a href="#about">Партнеру</a>
             </li>
-            <li>
+            <li className="hover:text-gallery-white hover:text-exstrabold">
               <a href="#contact">Локації</a>
             </li>
           </ul>
