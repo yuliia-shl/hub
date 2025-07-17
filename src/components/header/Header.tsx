ea2f54ddd4720f376c4859da52c718eaf6439706
kapillar <kapillarua@gmail.com>
2025-07-17 09:42:31 +0300

    style: add line-height 100% for nav-text

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
           </svg>
         </a>
         <nav className="xs:hidden 1xl:block">
-          <ul className="flex 1xl:gap-x-31 1xl:text-xl 3xl:gap-38 4xl:gap-52 4xl:text-2xl text-boulder-dust/77 font-bold items-center h-full">
+          <ul className="flex 1xl:gap-x-31 1xl:text-xl/5 3xl:gap-38 4xl:gap-52 4xl:text-2xl/6 text-boulder-dust/77 font-bold items-center h-full tracking-[-0.02em]">
             <li className="hover:text-gallery-white hover:text-exstrabold">
               <a href="#">Інвестору</a>
             </li>
