ab1c487ec7d8ad1e2fe4a6b14d183740f038489b
Kateryna <katerynawalls@proton.me>
2025-07-20 19:40:29 +0000

    fix: fix padding

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -3,8 +3,8 @@ import AskUsButton from '../AskUsButton/AskUsButton';
 
 const FAQ = () => {
   return (
-    <section className="pb-40 1xl:pb-55">
-      <div className="section flex flex-col pt-8.75 pb-7.5 1xl:pt-0 3xl:pt-0 4xl:pt-2.5 1xl:flex-row 1xl:justify-between 1xl:items-start 1xl:gap-24 4xl:gap-55">
+    <section className="pb-40 1xl:pb-55 3xl:pb-60 4xl:pb-70">
+      <div className="section flex flex-col pt-8.75 pb-7.5 1xl:pt-0 4xl:pt-2.5 1xl:flex-row 1xl:justify-between 1xl:items-start 1xl:gap-24 4xl:gap-55">
         {/* Left: h2 + desktop-only button */}
         <div className="flex flex-col 1xl:items-start 1xl:w-[362px] 3xl:w-[488px] 4xl:w-[534px] 1xl:gap-8.5 3xl:gap-12.5 4xl:gap-10.5 4xl:pt-2.5 mb-12 1xl:mb-0">
           <h2 className="text-[32px]/[110%] 1xl:text-[42px] 3xl:text-5xl 4xl:text-[62px] font-second font-medium text-center 1xl:text-left tracking-[-0.02em]">
