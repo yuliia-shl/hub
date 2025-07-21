6514def99c19224cecf491723faf54e4fd203509
kapillar <kapillarua@gmail.com>
2025-07-21 14:31:24 +0300

    style: edits font size in 4xl format

--- src/components/Provides/Provides.tsx
+++ src/components/Provides/Provides.tsx
@@ -5,7 +5,7 @@ export default function Provides() {
         <h2 className="font-second text-[34px]/[110%] text-mercury-white tracking-[-0.02em] font-medium mb-9.5 1xl:text-[42px] 3xl:text-[54px] 4xl:text-[62px] 1xl:mr-12 3xl:mr-16 4xl:mr-22 1xl:mb-0">
           Що надає HUB?
         </h2>
-        <ul className="flex flex-col gap-x-6 text-boulder-dust text-xl/[110%] mb-13.5 1xl:max-w-[900px] 1xl:flex-row 1xl:flex-wrap 1xl:gap-x-3 1xl:gap-y-12 3xl:gap-x-13 4xl:gap-x-18 3xl:max-w-[1142px] 3xl:mb-27 4xl:max-w-[1540px]">
+        <ul className="flex flex-col gap-x-6 text-boulder-dust text-xl/[110%] mb-13.5 1xl:max-w-[900px] 1xl:flex-row 1xl:flex-wrap 1xl:gap-x-3 1xl:gap-y-12 3xl:gap-x-13 3xl:max-w-[1142px] 3xl:mb-27 4xl:gap-x-18 4xl:text-[22px] 4xl:max-w-[1540px]">
           <li className="tracking-[0.02em] 3xl:tracking-[0.07em] 1xl:tracking-normal 1xl:max-w-111 3xl:max-w-[544px] 4xl:max-w-[732px]">
             Інноваційний ХАБ — це автоматичний комплекс для продажу товарів і
             послуг у локаціях з високим трафіком. 
@@ -19,7 +19,7 @@ export default function Provides() {
             провідних торгових марок в автоматичному режимі 24 на 7. 
           </li>
         </ul>
-        <ul className="flex flex-col gap-y-10.5 1xl:gap-x-3 text-lg/[156%] text-mercury-white font-semibold 1xl:font-bold 1xl:text-[22px]/[127%] px-4 1xl:flex-row 1xl:flex-wrap 1xl:max-w-[900px] 1xl:px-0  1xl:ml-auto 3xl:gap-x-13 4xl:gap-x-18 3xl:max-w-[1142px] 4xl:max-w-[1540px]">
+        <ul className="flex flex-col gap-y-10.5 1xl:gap-x-3 text-lg/[156%] text-mercury-white font-semibold 1xl:font-bold 1xl:text-[22px]/[127%] px-4 1xl:flex-row 1xl:flex-wrap 1xl:max-w-[900px] 1xl:px-0  1xl:ml-auto 3xl:gap-x-13 4xl:gap-x-18 3xl:max-w-[1142px] 4xl:max-w-[1540px] 4xl:text-[28px]/[136%]">
           <li className="flex gap-9.5 h-16 items-center 1xl:max-w-[444px] 3xl:max-w-[544px] 4xl:max-w-[732px] 1xl:shrink-1 grow-1 w-full">
             <svg className="w-8.5 h-8.5 fill-tangerine shrink-0">
               <use href="/images/svg/icons.svg#icon-8"></use>
