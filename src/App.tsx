0474a562459436e94d003fe35779d0a5df7e9605
kapillar <kapillarua@gmail.com>
2025-07-21 14:25:37 +0300

    feat: create components Provides/ add styles for them/load in home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './App.css';
 import Header from './components/header/Header';
 import Partners from './components/Partners/Partners';
 import Modules from './components/Modules/Modules';
+import Provides from './components/Provides/Provides';
 
 function App() {
   return (
     <>
       <Header />
+      <Provides />
       <Partners />
       <Modules />
     </>
---
+++ src/components/Provides/Provides.tsx
@@ -0,0 +1,69 @@
+export default function Provides() {
+  return (
+    <section className="pb-40 1xl:pb-55 3xl:pb-60 4xl:pb-70">
+      <div className="section flex flex-col 1xl:flex-row 1xl:flex-wrap">
+        <h2 className="font-second text-[34px]/[110%] text-mercury-white tracking-[-0.02em] font-medium mb-9.5 1xl:text-[42px] 3xl:text-[54px] 4xl:text-[62px] 1xl:mr-12 3xl:mr-16 4xl:mr-22 1xl:mb-0">
+          Що надає HUB?
+        </h2>
+        <ul className="flex flex-col gap-x-6 text-boulder-dust text-xl/[110%] mb-13.5 1xl:max-w-[900px] 1xl:flex-row 1xl:flex-wrap 1xl:gap-x-3 1xl:gap-y-12 3xl:gap-x-13 4xl:gap-x-18 3xl:max-w-[1142px] 3xl:mb-27 4xl:max-w-[1540px]">
+          <li className="tracking-[0.02em] 3xl:tracking-[0.07em] 1xl:tracking-normal 1xl:max-w-111 3xl:max-w-[544px] 4xl:max-w-[732px]">
+            Інноваційний ХАБ — це автоматичний комплекс для продажу товарів і
+            послуг у локаціях з високим трафіком. 
+          </li>
+          <li className="tracking-[0.02em] 3xl:tracking-[0.07em] 1xl:tracking-normal 1xl:max-w-111 3xl:max-w-[544px] 4xl:max-w-[732px] order-3">
+            Такі комплекси встановлюються в супермаркетах, ТРЦ, бізнес центрах,
+            вокзалах та в житлових комплексах.
+          </li>
+          <li className="tracking-[0.02em] 3xl:tracking-[0.07em] 1xl:tracking-normal 1xl:max-w-111 3xl:max-w-[544px] 4xl:max-w-[732px]">
+            В одному місці, користувач отримує комплекс товарів та послуг від
+            провідних торгових марок в автоматичному режимі 24 на 7. 
+          </li>
+        </ul>
+        <ul className="flex flex-col gap-y-10.5 1xl:gap-x-3 text-lg/[156%] text-mercury-white font-semibold 1xl:font-bold 1xl:text-[22px]/[127%] px-4 1xl:flex-row 1xl:flex-wrap 1xl:max-w-[900px] 1xl:px-0  1xl:ml-auto 3xl:gap-x-13 4xl:gap-x-18 3xl:max-w-[1142px] 4xl:max-w-[1540px]">
+          <li className="flex gap-9.5 h-16 items-center 1xl:max-w-[444px] 3xl:max-w-[544px] 4xl:max-w-[732px] 1xl:shrink-1 grow-1 w-full">
+            <svg className="w-8.5 h-8.5 fill-tangerine shrink-0">
+              <use href="/images/svg/icons.svg#icon-8"></use>
+            </svg>
+            <p>Хімчистка, ремонт одягу та аксесуарів</p>
+          </li>
+          <li className="flex gap-9.5 h-16 items-center 1xl:max-w-[444px] 3xl:max-w-[544px] 4xl:max-w-[732px] 1xl:w-full">
+            <svg className="w-8.5 h-8.5 fill-tangerine shrink-0">
+              <use href="/images/svg/icons.svg#icon-9"></use>
+            </svg>
+            <p>Реалізація оздоровчих товарів</p>
+          </li>
+          <li className="flex gap-9.5 h-16 items-center 1xl:max-w-[444px] 3xl:max-w-[544px] 4xl:max-w-[732px] 1xl:w-full">
+            <svg className="w-8.5 h-8.5 fill-tangerine shrink-0">
+              <use href="/images/svg/icons.svg#icon-11"></use>
+            </svg>
+            <p>Продаж ексклюзивних букетів та авторських композицій</p>
+          </li>
+          <li className="flex gap-9.5 h-16 items-center 1xl:max-w-[444px] 3xl:max-w-[544px] 4xl:max-w-[732px] 1xl:w-full">
+            <svg className="w-8.5 h-8.5 fill-tangerine shrink-0">
+              <use href="/images/svg/icons.svg#icon-6"></use>
+            </svg>
+            <p>Логістика: прийом та передача посилок</p>
+          </li>
+          <li className="flex gap-9.5 h-16 items-center 1xl:max-w-[444px] 3xl:max-w-[544px] 4xl:max-w-[732px] 1xl:w-full">
+            <svg className="w-8.5 h-8.5 fill-tangerine shrink-0">
+              <use href="/images/svg/icons.svg#icon-7"></use>
+            </svg>
+            <p>Прокат техніки та інструментів</p>
+          </li>
+          <li className="flex gap-9.5 h-16 items-center 1xl:max-w-[444px] 3xl:max-w-[544px] 4xl:max-w-[732px] 1xl:w-full">
+            <svg className="w-8.5 h-8.5 fill-tangerine shrink-0">
+              <use href="/images/svg/icons.svg#icon-10"></use>
+            </svg>
+            <p>Автоматична камера зберігання</p>
+          </li>
+          <li className="flex gap-9.5 h-16 items-center 1xl:max-w-[444px] 3xl:max-w-[544px] 4xl:max-w-[732px]">
+            <svg className="w-8.5 h-8.5 fill-tangerine shrink-0">
+              <use href="/images/svg/icons.svg#icon-12"></use>
+            </svg>
+            <p>Швидкі побутові послуги</p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
