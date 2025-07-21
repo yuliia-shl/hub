31d596c9a9b10423016960eb7d20b2d2b3041534
kapillar <kapillarua@gmail.com>
2025-07-21 15:44:00 +0300

    style: edits width for <p>/and other

--- src/components/ui/Button/SmartButton.tsx
+++ src/components/ui/Button/SmartButton.tsx
@@ -30,10 +30,11 @@ export default function SmartButton({
   variant = 'primary',
 }: SmartButtonProps) {
   const baseClasses =
-    'font-semibold py-2 px-4 rounded-full border-star-dust-400 text-star-dust-400 hover:text-tangerine hover:border-tangerine transition-colors border-1 inline-flex items-center justify-center focus:outline-tangerine focus:outline-2 focus:text-tangerine py-4 4xl:py-[21px] px-6 4xl:px-6 rounded-full duration-300 w-fit';
+    'font-semibold py-2 px-4 rounded-full border-star-dust-400 text-star-dust-400 hover:text-tangerine hover:border-tangerine transition-colors border-1 inline-flex items-center justify-center focus:outline-tangerine focus:outline-2 focus:text-tangerine 4xl:py-4 4xl:py-[21px]4xl:px-6 duration-300 w-fit';
 
   const variantClasses = {
-    primary: 'bg-woodsmoke-dimmed',
+    primary:
+      'bg-cod-gray 1xl:bg-woodsmoke-dimmed border-tangerine 1xl:border-star-dust-400 text-tangerine 1xl:text-star-dust-400 ',
     secondary: 'bg-transparent hover:text-white',
     danger:
       'bg-red-100 text-red-700 border-red-300 hover:bg-red-500 hover:text-white',
