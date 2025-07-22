e293ecf5c3e37c4b278a9fe0c2063b7194c992b3
Yuli <yu.expert@gmail.com>
2025-07-23 01:58:43 +0300

    chore: clean code

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,17 +16,17 @@ const Header = () => {
           </a>
           <nav className="hidden 1xl:block  1xl:px-15 4xl:px-14 1xl:border-1 4xl:border-1.29 border-woodsmoke-black rounded-full bg-woodsmoke-black/50 backdrop-blur-[4px] max-h-full h-full items-stretch">
             <ul className="flex 1xl:gap-x-31 1xl:text-xl/5 3xl:gap-38 4xl:gap-52 4xl:text-2xl/6 text-boulder-dust/77 font-bold items-center h-full tracking-[-0.02em]">
-              <li className="hover:text-gallery-white hover:text-exstrabold transition-colors">
+              <li className="hover:text-gallery-white transition-colors">
                 <a href="#" className="py-3">
                   Інвестору
                 </a>
               </li>
-              <li className="hover:text-gallery-white hover:text-exstrabold transition-colors">
+              <li className="hover:text-gallery-white transition-colors">
                 <a href="#about" className="py-3">
                   Партнеру
                 </a>
               </li>
-              <li className="hover:text-gallery-white hover:text-exstrabold transition-colors">
+              <li className="hover:text-gallery-white transition-colors">
                 <a href="#contact" className="py-3">
                   Локації
                 </a>
@@ -38,7 +38,6 @@ const Header = () => {
               type="button"
               onClick={() => setOpenBookModal(true)}
               className="flex shrink-1 text-nowrap mr-5.5 1xl:mr-0 items-center justify-center-safe px-8 1xl:px-15 py-2.5 1xl:py-4.5 4xl:py-5.5 border-1 4xl:border-1.29 border-masala-light 1xl:border-star-dust rounded-full w-fit max-h-10 max-w-full 1xl:max-h-13 4xl:max-h-16 text-lg/4.5 1xl:text-base/4 4xl:text-xl/5 1xl:font-semibold text-star-dust hover:border-tangerine transition-colors hover:text-tangerine hover:bg-fire hover:shadow-lg"
-              // onClick={() => setOpenLoginModal(true)}
             >
               Замовити консультацію
             </button>
@@ -58,6 +57,7 @@ const Header = () => {
           </div>
         </div>
       </header>
+
       <Modal isOpen={openBookModal} onClose={() => setOpenBookModal(false)}>
         <BookModal />
       </Modal>
