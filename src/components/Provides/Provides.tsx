c1ae19bed1057669ad2c36f6b80ff0ad654b5cb4
kapillar <kapillarua@gmail.com>
2025-07-23 07:44:23 +0300

    chore: edits gap in ul

--- src/components/Provides/Provides.tsx
+++ src/components/Provides/Provides.tsx
@@ -2,10 +2,10 @@ export default function Provides() {
   return (
     <section className="pb-40 1xl:pb-55 3xl:pb-60 4xl:pb-70">
       <div className="section flex flex-col 1xl:flex-row 1xl:flex-wrap">
-        <h2 className="font-second text-[34px]/[110%] text-mercury-white tracking-[-0.02em] font-medium mb-9.5 1xl:text-[42px] 3xl:text-[54px] 4xl:text-[62px] 1xl:mr-8 3xl:mr-16 4xl:mr-22 1xl:mb-0">
+        <h2 className="font-second text-[34px]/[110%] text-mercury-white tracking-[-0.02em] font-medium mb-9.5 1xl:text-[42px] 3xl:text-[54px] 4xl:text-[62px] 1xl:mr-11 3xl:mr-17.5 4xl:mr-23 1xl:mb-0">
           Що надає HUB?
         </h2>
-        <ul className="flex flex-col gap-y-6 text-boulder-dust text-xl/[110%] mb-13.5 1xl:max-w-[904px] 1xl:flex-row 1xl:flex-wrap 1xl:gap-x-4.5 1xl:gap-y-13.5  3xl:max-w-[1142px] 1xl:mb-27 4xl:text-[22px] 4xl:max-w-[1540px] 4xl:gap-y-16">
+        <ul className="flex flex-col gap-y-6 text-boulder-dust text-xl/[110%] mb-13.5 1xl:max-w-[904px] 1xl:flex-row 1xl:flex-wrap 1xl:gap-x-4 1xl:gap-y-13.5  3xl:max-w-[1142px] 1xl:mb-27 4xl:text-[22px] 4xl:max-w-[1540px] 3xl:gap-x-13 4xl:gap-x-18 4xl:gap-y-16">
           <li className="tracking-[0.02em] 3xl:tracking-[0.07em] 1xl:tracking-normal 1xl:max-w-[440px] 3xl:max-w-[544px] 4xl:max-w-[732px]">
             Інноваційний ХАБ — це автоматичний комплекс для продажу товарів і
             послуг у локаціях з високим трафіком. 
@@ -19,7 +19,7 @@ export default function Provides() {
             провідних торгових марок в автоматичному режимі 24 на 7. 
           </li>
         </ul>
-        <ul className="flex flex-col gap-y-10.5 1xl:gap-x-3 text-lg/[156%] text-mercury-white font-semibold 1xl:font-bold 1xl:text-[22px]/[127%] 1xl:flex-row 1xl:flex-wrap 1xl:max-w-[904px] 1xl:ml-auto 3xl:gap-x-13 4xl:gap-x-18 3xl:max-w-[1142px] 4xl:max-w-[1540px] 4xl:text-[28px]/[136%]">
+        <ul className="flex flex-col gap-y-10.5 1xl:gap-x-4 text-lg/[156%] text-mercury-white font-semibold 1xl:font-bold 1xl:text-[22px]/[127%] 1xl:flex-row 1xl:flex-wrap 1xl:max-w-[904px] 1xl:ml-auto 3xl:gap-x-13 4xl:gap-x-18 3xl:max-w-[1142px] 4xl:max-w-[1540px] 4xl:text-[28px]/[136%]">
           <li className="flex gap-6 h-16 items-center 1xl:max-w-[444px] 3xl:max-w-[544px] 4xl:max-w-[732px] w-full">
             <span className="flex justify-center items-center w-16 h-16 rounded-full bg-woodsmoke-dark shrink-0">
               <svg className="flex w-8.5 h-8.5 fill-tangerine shrink-0">
