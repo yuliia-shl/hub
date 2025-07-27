89de93ab6213f40d4f29639c467ebad22830271b
Yuli <yu.expert@gmail.com>
2025-07-27 20:27:22 +0300

    fix(reasons): update image and fix photo positioning

--- src/components/Reasons/Reasons.tsx
+++ src/components/Reasons/Reasons.tsx
@@ -1,8 +1,8 @@
 const Reasons = () => {
   return (
     <section id="reasons" className="pb-40 1xl:pb-55 3xl:pb-60 4xl:pb-70">
-      <div className="section flex flex-col 1xl:flex-row-reverse gap-[108px] 1xl:gap-4 3xl:gap-[122px] 4xl:gap-[108px]">
-        <div className="w-full shrink-1 4xl:pt-13 4xl:max-w-[1484px]">
+      <div className="section flex flex-col 1xl:flex-row-reverse gap-[108px] 1xl:gap-4 3xl:gap-[122px] 4xl:gap-[108px] 4xl:justify-end">
+        <div className="w-full shrink-1 4xl:pt-13 4xl:max-w-[1370px]">
           <h2 className="mb-14.5 1xl:mb-20 3xl:mb-17 4xl:mb-15 font-second text-[32px]/[110%] 1xl:text-[42px] 3xl:text-[54px] 4xl:text-[62px] font-medium text-center 1xl:text-left 1xl:ml-1 3xl:ml-0">
             Чому саме MVK MASH?
           </h2>
@@ -52,19 +52,12 @@ const Reasons = () => {
           </ul>
         </div>
 
-        <div className="relative max:w-[520px] 1xl:w-[440px] 3xl:w-[460px] 4xl:w-[646px] shrink-0">
+        <div className="max:w-[520px] 1xl:pt-12.5 1xl:w-[440px] 3xl:pt-3 3xl:w-[460px] 4xl:pl-[94px] 4xl:pt-0 4xl:w-[648px] 4xl:items-start shrink-0">
           <img
             alt="Smart Locker HUB"
-            src="/images/smart-locker.png"
-            className="absolute top-[-8px] xs:top-[-16px] 1xl:top-15 3xl:top-7 4xl:top-3.5 left-1 xs:left-10 1xl:left-8 3xl:left-8.5 4xl:left-21.5 max-w-[240px] xs:max-w-[286px] 1xl:w-[244px] 3xl:w-[254px] 4xl:w-[306px] xs:h-[330px] 1xl:h-[280px] 3xl:h-[294px] 4xl:h-[352px] rotate-[-6deg] opacity-[0.26]"
+            src="/images/smart-locker.webp"
+            className="w-full object-cover"
           />
-          <img
-            alt="Smart Locker HUB"
-            src="/images/smart-locker.png"
-            className="absolute top-[180px] xs:top-[206px] 1xl:top-[248px] 3xl:top-[226px] 4xl:top-[254px] left-10 xs:left-1/2 xs:-translate-x-1/2 max-w-[300px] xs:max-w-[362px] 1xl:w-[308px] 3xl:w-[322px] 4xl:w-[386px] xs:h-[417px] 1xl:h-[354px] 3xl:h-[370px] 4xl:h-[445px] rotate-[29deg] z-2"
-          />
-          {/* Spacer для забезпечення висоти секції */}
-          <div className="invisible w-full min-h-[526px] xs:min-h-[596px] 1xl:h-[660px] 3xl:h-[638px] 4xl:h-[710px] 1xl:pt-12.5 3xl:pt-0 4xl:pl-23" />
         </div>
       </div>
     </section>
