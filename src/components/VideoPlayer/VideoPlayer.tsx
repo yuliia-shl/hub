383f15221c22ae93ee39ea6531953d017f9dc589
kapillar <kapillarua@gmail.com>
2025-07-31 22:32:53 +0300

    chore: add mask to video & add on the top text from video for mobile

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -3,7 +3,7 @@ import SmartButton from '../ui/Button/SmartButton';
 
 const VideoPlayer = () => {
   return (
-    <div className="relative -mt-17 1xl:-mt-10.5 3xl:-mt-18 4xl:-mt-17 w-full h-screen overflow-hidden bg-woodsmoke-black/75">
+    <div className="relative -mt-17 1xl:-mt-10.5 3xl:-mt-18 4xl:-mt-17 w-full h-screen overflow-hidden bg-[linear-gradient(to_top,_#080808_0%,_#080808_10%,_rgba(8,8,8,0.5)_100%)]">
       <video
         className="absolute top-0 left-0 z-[-1] w-full h-full object-cover"
         autoPlay
