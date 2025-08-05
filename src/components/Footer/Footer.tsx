f0d5bcc507c85a54520264f946a1a6a748e1d6c3
Kateryna <katerynawalls@proton.me>
2025-08-05 22:26:44 +0000

    feat(footer): add responsive layout for 375px and 1024px breakpoints

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,31 +4,31 @@
 
 const Footer = ({ onPolicyClick} : FooterProps) => {
   return (
-    <footer className="w-full pb-[7px] lg:pb-5 1xl:pb-14.5 4xl:pb-17">
+    <footer className="w-full pb-18.5 xs:pb-[7px] lg:pb-5 1xl:pb-14.5 4xl:pb-17">
       <div className="section">
-        <div className="pb-17 lg:pb-5 4xl:pb-6.5">
-          <div className="flex flex-row justify-between 1xl:justify-none mb-15 lg:mb-8 4xl:mb-15">
-            <div className="flex flex-row items-start  gap-x-[6px] lg:gap-x-2.5 4xl:gap-x-3">
+        <div className="pt-0 pb-0 xs:pb-17 lg:pb-5 4xl:pb-6.5">
+          <div className="flex flex-row justify-between 1xl:justify-none mb-10.5 xs:mb-15 lg:mb-8 4xl:mb-15">
+            <div className="flex flex-row items-start  gap-x-[5px] xs:gap-x-[6px] lg:gap-x-2.5 4xl:gap-x-3">
               <a
                 href="/"
                 aria-label="На головну"
               >
-                <svg className="w-[36px] h-[50px] lg:w-[56px] lg:h-[78px] 4xl:w-[68px] 4xl:h-[96px] fill-tangerine group-hover:fill-tangerine transition-colors duration-300">
+                <svg className="w-[30px] h-[42px] xs:w-[36px] xs:h-[50px] lg:w-[56px] lg:h-[78px] 4xl:w-[68px] 4xl:h-[96px] fill-tangerine group-hover:fill-tangerine transition-colors duration-300">
                   <use href="/images/svg/icons.svg#icon-logo" />
                 </svg>
               </a>
 
-              <div className="flex flex-col pt-2 lg:pt-3 4xl:pt-5">
-                <span className="text-xl/[120%] lg:text-[32px] 4xl:text-[38px] text-tangerine font-semibold font-second">
+              <div className="flex flex-col pt-1.5 xs:pt-2 lg:pt-3 4xl:pt-5">
+                <span className="leading-[120%] xs:text-xl lg:text-[32px] 4xl:text-[38px] text-tangerine font-semibold font-second">
                   MBK MASH
                 </span>
-                <span className="text-sm/[120%] lg:text-[22px] 4xl:text-[26px] text-white font-second">
+                <span className="text-[11px] leading-[120%] xs:text-sm lg:text-[22px] 4xl:text-[26px] text-white font-second">
                   SMART LOCKERS
                 </span>
               </div>
             </div>
                       {/*Cоціальні мережі*/}
-            <div className="flex gap-x-4 lg:gap-x-5 lg:pt-4 lg:w-1/2 4xl:gap-x-14">
+            <div className="flex gap-x-3 xs:gap-x-4 lg:gap-x-5 lg:pt-4 lg:w-1/2 4xl:gap-x-14">
               <a
                 href="https://t.me/HUB_mvk_mash"
                 target="_blank"
@@ -36,7 +36,7 @@ const Footer = ({ onPolicyClick} : FooterProps) => {
                 aria-label="Telegram"
                 className="group"
               >
-                <svg className="w-12 h-12 lg:w-14 lg:h-14 4xl:w-20.5 4xl:h-20.5 fill-tangerine">
+                <svg className="w-10.5 h-10.5 xs:w-12 xs:h-12 lg:w-14 lg:h-14 4xl:w-20.5 4xl:h-20.5 fill-tangerine">
                   <use href="/images/svg/icons.svg#icon-telegram" />
                 </svg>
               </a>
@@ -47,7 +47,7 @@ const Footer = ({ onPolicyClick} : FooterProps) => {
                 aria-label="Instagram"
                 className="group"
               >
-                <svg className="w-12 h-12 lg:w-14 lg:h-14 4xl:w-20.5 4xl:h-20.5 fill-tangerine">
+                <svg className="w-10.5 h-10.5 xs:w-12 xs:h-12 lg:w-14 lg:h-14 4xl:w-20.5 4xl:h-20.5 fill-tangerine">
                   <use href="/images/svg/icons.svg#icon-insta" />
                 </svg>
               </a>
@@ -58,7 +58,7 @@ const Footer = ({ onPolicyClick} : FooterProps) => {
                 aria-label="Tik-Tok"
                 className="group"
               >
-                <svg className="w-12 h-12 1xl:w-14 1xl:h-14 4xl:w-20.5 4xl:h-20.5 fill-tangerine">
+                <svg className="w-10.5 h-10.5 xs:w-12 xs:h-12 lg:w-14 lg:h-14 4xl:w-20.5 4xl:h-20.5 fill-tangerine">
                   <use href="/images/svg/icons.svg#icon-Union" />
                 </svg>
               </a>
@@ -67,12 +67,12 @@ const Footer = ({ onPolicyClick} : FooterProps) => {
 
           <div className="flex flex-col lg:flex-row">
             <div className="flex flex-col lg:order-2 lg:self-end lg:w-1/2">
-              <h3 className="self-start leading-[120%] 4xl:text-xl/[120%] font-medium font-second mb-4.5 4xl:mb-6">
+              <h3 className="self-start leading-[120%] 4xl:text-xl/[120%] font-medium font-second mb-6 xs:mb-4.5 4xl:mb-6">
                 Надсилайте свої запитання та пропозиції
               </h3>
 
-              <div className="flex flex-row mb-13 4xl:mb-0">
-                <div className="flex flex-col gap-y-[5px] flex-1 3xl:flex-none 3xl:mr-34 4xl:mr-50 items-start">
+              <div className="flex flex-col xs:flex-row mb-9 xs:mb-13 4xl:mb-0">
+                <div className="flex flex-col gap-y-[2px] xs:gap-y-[5px] flex-1 mb-3 xs:mb-0 3xl:flex-none 3xl:mr-34 4xl:mr-50 items-start">
                   <p className="font-medium leading-[120%] 4xl:text-xl/[120%] text-mountain-mist">
                     Запитання:
                   </p>
@@ -97,7 +97,7 @@ const Footer = ({ onPolicyClick} : FooterProps) => {
             </div>
 
             <div className="flex flex-col lg:w-1/2 4xl:flex-row">
-              <div className="flex flex-row 4xl:order-2 mb-9.5 lg:mb-8 4xl:mb-0  1xl:whitespace-nowrap">
+              <div className="flex flex-row 4xl:order-2 mb-11 xs:mb-9.5 lg:mb-8 4xl:mb-0  1xl:whitespace-nowrap">
                 <a
                   href="#"
                   className="w-1/2 text-lg/[126%] tracking-[-0.03em] text-mountain-mist underline"
@@ -110,15 +110,12 @@ const Footer = ({ onPolicyClick} : FooterProps) => {
                 </a>
               </div>
 
-              <div className="flex flex-col gap-4 lg:max-w-[296px] 1xl:max-w-[280px]">
+              <div className="flex flex-col gap-4 lg:max-w-[296px] 1xl:max-w-[280px] 4xl:mr-24">
                 <p className="leading-[120%] font-medium text-mountain-mist">
                   © 2003–2025 ТОВ «MBK MASH»
                 </p>
                 <p className="leading-[120%] font-medium text-mountain-mist lg:text-left">
-                  Всі права захищені. Використання матеріалів сайту{' '}
-                  <span className="block lg:inline">
-                    можливе тільки з посиланням на джерело.
-                  </span>
+                  Всі права захищені. Використання матеріалів сайту можливе тільки з посиланням на джерело.
                 </p>
               </div>
             </div>
